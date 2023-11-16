9d6f2f5938f194e323a5c34f193a13ca874ce54e
MTsocketAPI <nengkya@yahoo.com>
2023-11-16 17:25:54 +0000

    #define SOL_SOCKET 0xffff; = 65535; options for SOcket Level
    https://github.com/openbsd/src/blob/master/sys/sys/socket.h

--- server.c
+++ server.c
@@ -1,15 +1,20 @@
 /*
-#define AF_INET 2; Internet IP Protocol; Address Family InterNET
+#define AF_INET 2; Internet Protocol (IP); Address Family InterNET
+#define	SOL_SOCKET 0xffff; = 65535; options for socket level or SOcket Layer
+
+int socket(int domain, int type, int protocol);
 */
-#include <sys/socket.h> /* socket(); */
-#include <stdio.h>	/* perror(); */
-#include <stdlib.h>	/* EXIT_FAILURE */
+#include <sys/socket.h>
+#include <stdio.h>  /* perror(); */
+#include <stdlib.h> /* EXIT_FAILURE */
 
 int main(){
 
     int server_file_descriptor;
     
     /*
+    1. Create socket
+
     Transmission Control Protocol (TCP) almost always uses SOCK_STREAM.
     User Datagram Protocol (UDP) uses SOCK_DGRAM.
 
@@ -33,12 +38,41 @@ int main(){
 
     Bank use TCP.
     Online game use UDP.
+
+    int socket(int domain, int type, int protocol);
+
+    domain; Use AF_ LOCAL as defined in the POSIX standard for communication
+    between processes on the same host.
+    For communicating between processes on different hosts connected by IPV4, use AF_INET.
+
+    int protocol; Specifies a particular protocol to be used with the socket.
+    Specifying a protocol of 0 causes socket() to use an unspecified default protocol
+    appropriate for the requested socket type.
+
+    warning: suggest parentheses around assignment used as truth value [-Wparentheses]
+    if (server_file_descriptor = socket(AF_INET, SOCK_STREAM, 0) < 0){
     */
-    if (server_file_descriptor = socket(AF_INET, SOCK_STREAM, ) < 0){
+    if ((server_file_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 	/* print error */
 	perror("socket failed.");
 	/* The value of EXIT_FAILURE is defined in stdlib. h as 8; */
 	exit(EXIT_FAILURE);
+    }
+
+    /*
+    2. Set socket option
+
+    This helps in manipulating options for the socket referred by the file descriptor sockfd.
+    This is completely optional, but it helps in reuse of address and port.
+    Prevents error such as: “address already in use”.
+
+    int setsockopt(int sockfd, int level, int optname,  const void * option_value, socklen_t optlen);
+
+    Forcefully attaching 1. Socket to the port 8000.
+    */
+    if (setsockopt(server_file_descriptor, SOL_SOCKET, )){
+
+
     }
 
 }
